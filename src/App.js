f5183306823495461b48cf2a4721644d37937ff4
Justin Bealer <jmbealer11@gmail.com>
2022-08-12 13:00:54 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./App.css";
+import RecipeCreate from "./RecipeCreate";
+import RecipeList from "./RecipeList";
+import RecipeData from "./RecipeData"
+
+function App() {
+  const [recipes, setRecipes] = useState(RecipeData);
+
+  // TODO: Add the ability for the <RecipeList /> component to list and delete an existing recipe.
+  // TODO: Add the ability for the <RecipeCreate /> component to create new recipes.
+
+ // const addRecipe = (newRecipe) => 
+  //    setRecipes((currRecipes) => [
+   //     newRecipe,
+    //    ...currRecipes,
+     // ])
+  const addRecipe = (data) =>{
+    setRecipes([...recipes, data])
+  }
+ // const deleteRecipe = (indexToDelete) =>
+  //    setRecipes((currRecipes) =>
+   //     currRecipes.filter((post, index) => index !== indexToDelete)
+    //    );
+  
+  const deleteRecipe = (targetIndex) => {
+    setRecipes(recipes.filter((_, index) => index !== targetIndex))
+  }
+  
+  return (
+    <div className="App">
+      <header><h1>Delicious Food Recipes</h1></header>
+      <RecipeList recipes={recipes} deleteRecipe={deleteRecipe} />
+      <RecipeCreate addRecipe={addRecipe} />
+    </div>
+  );
+}
+
+export default App;
